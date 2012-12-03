18c7c6fbf0ef1990cd9da345490f0d1b7e8b2cad
hoamon <hoamon@ibmgrace>
2012-12-03 15:48:31 +0800

    replace main.py

--- trunk/wsgi.py
+++ trunk/wsgi.py
@@ -13,7 +13,9 @@ middleware here, or combine a Django application with an application of another
 framework.
 
 """
-import os
+import os, sys
+ROOT = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, ROOT)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
