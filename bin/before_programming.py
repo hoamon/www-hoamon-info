08a04b9b5fcb4f55da0e6cfe1de36ec583fde581
hoamon <hoamon@ibmgrace>
2012-12-05 17:03:38 +0800

    locate to project root directory

--- bin/before_programming.py
+++ bin/before_programming.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf8 -*-
+import os
 from advance_hg import AdvanceHG
 class Rargs:
     def __init__(self, rargs=[]): self.rargs = rargs
 
 ahg = AdvanceHG(run_in_function=True)
+pwd = os.path.dirname(os.path.abspath(__file__))
 a = Rargs(['-j'])
-ahg.pullAll('', '--pullall', '.', a)
+ahg.pullAll('', '--pullall', os.path.join(pwd, '..'), a)
