319faf781eeae14c652838f75b40513e16cb5a26
hoamon <hoamon@ibmgrace>
2012-12-28 23:18:33 +0800

    monitor sphinx directory, if any file changed, then "make html"

---
+++ bin/monitor_file_and_make_html.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd $1
+
+OLD=`stat -t ./*|grep -v "_build"|sort`
+# OLD=`stat -t testdir | sed 's/[0-9][0-9]*$//'`
+# if SE linux stat
+while true; do
+    NEW=`stat -t ./*|grep -v "_build"|sort`
+    # NEW=`stat -t testdir | sed 's/[0-9][0-9]*$//'` if SE linux stat
+    [ "$NEW" == "$OLD" ] || make html
+    OLD=$NEW
+    sleep 3
+done
