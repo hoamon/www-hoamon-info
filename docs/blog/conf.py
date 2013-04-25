777146ed69c26a8558a5df9dbbd76f69c520f0a0
hoamon <hoamon@Acer>
2013-04-25 22:31:14 +0800

    simple copyright

--- docs/blog/conf.py
+++ docs/blog/conf.py
@@ -17,7 +17,7 @@ tagline = '"sandbox" is a jargon of Version Control System.'
 author = 'Ho, Yueh-Feng &lt;<a href="http://www.hoamon.info/">http://<b>hoamon</b>.info/</a>&gt;'
 
 # Change this to your copyright string
-copyright = '2006-2012, ' + author
+copyright = '2006-2013, Yueh-Feng Ho'# + author
 
 # Change this to your blog root URL (required for RSS feed)
 website = 'http://www.hoamon.info/blog/'
