ab2c48eb67965a9280f4ffccfda00da12a9f1c70
hoamon <hoamon@Acer>
2013-04-26 09:49:42 +0800

    auto generate today year

--- docs/blog/conf.py
+++ docs/blog/conf.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import tinkerer
+import tinkerer, datetime
 import tinkerer.paths
 
 # **************************************************************
@@ -17,7 +17,7 @@ tagline = '"sandbox" is a jargon of Version Control System.'
 author = 'Ho, Yueh-Feng &lt;<a href="http://www.hoamon.info/">http://<b>hoamon</b>.info/</a>&gt;'
 
 # Change this to your copyright string
-copyright = '2006-2013, Yueh-Feng Ho'# + author
+copyright = '2006-%s, Yueh-Feng Ho(hoamon)'%datetime.datetime.today().year
 
 # Change this to your blog root URL (required for RSS feed)
 website = 'http://www.hoamon.info/blog/'
