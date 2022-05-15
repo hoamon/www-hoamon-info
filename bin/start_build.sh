4260057d8c63abb3b5f52f6cce3d236c312a5834
hoamon h97progamer <hoamon@ho600.com>
2022-05-15 13:28:50 +0800

    Use variable to set ${ids} @master]

--- bin/start_build.sh
+++ bin/start_build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-aws --profile for-all-codebuild-project-in-hoamon-info-AWS codebuild start-build --project-name cp-files-to-AWS-s3-bucket-www-hoamon-info
-# aws --profile for-all-codebuild-project-in-hoamon-info-AWS codebuild batch-get-builds --ids cp-files-to-AWS-s3-bucket-www-hoamon-info:b1edbdb9-ab30-4986-8d7e-2545401a56ef | jq ".builds[].buildStatus"
+ids=`aws --profile for-all-codebuild-project-in-hoamon-info-AWS codebuild start-build --project-name cp-files-to-AWS-s3-bucket-www-hoamon-info | jq ".build.id"`
+# aws --profile for-all-codebuild-project-in-hoamon-info-AWS codebuild batch-get-builds --ids $(sed 's/"//g' <<< $ids) | jq ".builds[].buildStatus"
